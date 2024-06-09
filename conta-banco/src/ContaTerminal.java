804e3abc88c2488f169c688900bbce31938c320e
rafaalbino <rafaalbino8@gmail.com>
2024-06-09 18:49:59 -0300

    primeira versão

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o numero da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o numero da agencia:");
+        String agencia = scanner.next();
+
+        System.out.print("Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.print("Digite o numero do saldo:");
+        float saldo = scanner.nextFloat();
+
+        System.out.printf("Ola %s, obrigado por criar uma conta em nosso banco, sua agencia eh %s, conta %d e seu saldo %.2f ja esta disponavel para saque",nome,agencia,numero,saldo);
     }
 }
